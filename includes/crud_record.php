dc6eafad5fc0c8b824a6df6d83fede6a1f80b80e
anzurna <aversatrix@mail.ru>
2021-08-09 10:42:18 +0300

    layout changes. sample crud added

---
+++ includes/crud_record.php
@@ -0,0 +1,51 @@
+<div id="crud-user" id="crud-user">
+  <div class="container-fluid">
+    <form method="POST"  class="col-md-4">
+      <div class="form-group">
+        <label for="inputFirstName">First name</label>
+        <input type="text" class="form-control" id="inputFirstName" placeholder="Enter your first name">
+      </div>
+      <div class="form-group">
+        <label for="inputLastName">Last name</label>
+        <input type="text" class="form-control" id="inputLastName" placeholder="Enter your last name">
+      </div>
+      <button type="submit" class="btn btn-primary">Add user</button>
+    </form>
+
+    <form method="POST"  class="col-md-4">
+      <div class="form-group">
+        <label for="inputFirstName">First name</label>
+        <input type="text" class="form-control" id="inputFirstName" placeholder="Enter your first name">
+      </div>
+      <div class="form-group">
+        <label for="inputLastName">Last name</label>
+        <input type="text" class="form-control" id="inputLastName" placeholder="Enter your last name">
+      </div>
+      <button type="submit" class="btn btn-primary">Add user</button>
+    </form>
+
+    <form method="POST"  class="col-md-4">
+      <div class="form-group">
+        <label for="inputFirstName">First name</label>
+        <input type="text" class="form-control" id="inputFirstName" placeholder="Enter your first name">
+      </div>
+      <div class="form-group">
+        <label for="inputLastName">Last name</label>
+        <input type="text" class="form-control" id="inputLastName" placeholder="Enter your last name">
+      </div>
+      <button type="submit" class="btn btn-primary">Add user</button>
+    </form>
+
+    <form method="POST"  class="col-md-4">
+      <div class="form-group">
+        <label for="inputFirstName">First name</label>
+        <input type="text" class="form-control" id="inputFirstName" placeholder="Enter your first name">
+      </div>
+      <div class="form-group">
+        <label for="inputLastName">Last name</label>
+        <input type="text" class="form-control" id="inputLastName" placeholder="Enter your last name">
+      </div>
+      <button type="submit" class="btn btn-primary">Add user</button>
+    </form>
+  </div>
+</div>
--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 <footer class="page-footer font-small blue footer">
 
     <!-- Copyright -->
-    <div class="footer-copyright text-center py-3">© 2021 Copyright:
+    <div class="footer-copyright text-center py-3">© <?php print date("Y");?> Copyright:
     
     </div>
     <!-- Copyright -->
